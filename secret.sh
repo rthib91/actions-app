c75b4d1129802e36f0bdd1d58dbdfc7e4583f1c4
rethibau <remi.thibault@intact.net>
2020-10-26 10:29:36 -0400

    ls

--- secret.sh
+++ secret.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SUPER_SECRET = $1
+SUPER_SECRET=$1
 
+# Trying to echo secret
 echo "$SUPER_SECRET"
 
+# Validating secret
 [[ "$SUPER_SECRET" == "secret value from secret" ]] && echo "Secret is good" || echo "Secret is not good"
