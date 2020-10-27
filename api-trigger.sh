bf454868cd6b7e9c7d12c51fbac4e39aaad690f8
rethibau <remi.thibault@intact.net>
2020-10-27 16:18:15 -0400

    token

--- api-trigger.sh
+++ api-trigger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl \
-  -u "rthib91":"$API_TOKEN"
+  -u ":$API_TOKEN"
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/rthib91/actions-app/actions/workflows/workflow.yml/dispatches \
